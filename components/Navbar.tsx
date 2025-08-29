c391bc34a3dc784da074880ec0aade0a6d695e51
Abdelrahman Elashmawy <abdo.ashmawy573@gmail.com>
2025-08-29 09:00:43 +0300

    Add stylized dividers around logo in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,21 @@ export function Navbar() {
   return (
     <header className="border-b border-gray-200">
       <div className="container-max py-4 flex items-center justify-between">
-        <Link href="/" className="text-xl font-semibold">
-        <Image src="/assets/logo.png" alt="AshPhys" width={140} height={100} />
-        </Link>
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <div className="h-px bg-black w-16 relative">
+              <div className="absolute right-0 top-0 w-px h-4 bg-black"></div>
+            </div>
+            <Link href="/" className="text-xl font-semibold mx-4">
+              <Image src="/assets/logo.png" alt="AshPhys" width={140} height={100} />
+            </Link>
+            <div className="flex items-center">
+              <div className="h-px bg-black w-16 relative">
+                <div className="absolute left-0 top-0 w-px h-4 bg-black"></div>
+              </div>
+            </div>
+          </div>
+        </div>
         <nav className="flex items-center gap-4">
           <Link className="text-sm hover:underline" href="/">Home</Link>
           <Link className="text-sm hover:underline" href="/about">About Us</Link>
