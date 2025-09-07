ec088d9b5fdcfe421d8cb9d84e61107f1ea928d3
Abdelrahman Elashmawy <abdo.ashmawy573@gmail.com>
2025-09-07 16:24:06 +0300

    Fix navbar merge conflict and update Book button to Book Now

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,37 +2,18 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export function Navbar() {
-
   return (
     <header className="border-b border-gray-200">
       <div className="container-max py-4 flex items-center justify-between">
-<<<<<<< Updated upstream
-        <div className="flex items-center">
-          <div className="flex items-center">
-            <div className="h-px bg-black w-16 relative">
-              <div className="absolute right-0 top-0 w-px h-4 bg-black"></div>
-            </div>
-            <Link href="/" className="text-xl font-semibold mx-4">
-              <Image src="/assets/logo.png" alt="AshPhys" width={140} height={100} />
-            </Link>
-            <div className="flex items-center">
-              <div className="h-px bg-black w-16 relative">
-                <div className="absolute left-0 top-0 w-px h-4 bg-black"></div>
-=======
-        <div className="flex items-center gap-4">
-          <div className="flex items-center">
-            <div className="h-px bg-black flex-1 mr-2 relative">
-              <div className="absolute right-0 top-0 w-px h-3 bg-black"></div>
-            </div>
-            <Link href="/" className="text-xl font-semibold">
-              <Image src="/assets/logo.png" alt="AshPhys" width={140} height={100} />
-            </Link>
-            <div className="flex items-center">
-              <div className="h-px bg-black flex-1 ml-2 relative">
-                <div className="absolute left-0 top-0 w-px h-3 bg-black"></div>
->>>>>>> Stashed changes
-              </div>
-            </div>
+        <div className="flex items-center flex-1 mr-4">
+          <div className="bg-black w-16 relative" style={{ height: '2.7px' }}>
+            <div className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black" style={{ width: '2.7px', height: '10px' }}></div>
+          </div>
+          <Link href="/" className="text-xl font-semibold mx-1">
+            <Image src="/assets/logo.png" alt="AshPhys" width={140} height={100} />
+          </Link>
+          <div className="bg-black flex-1 relative" style={{ height: '2.7px' }}>
+            <div className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black" style={{ width: '2.7px', height: '10px' }}></div>
           </div>
         </div>
         <nav className="flex items-center gap-4">
@@ -40,6 +21,7 @@ export function Navbar() {
           <Link className="text-sm hover:underline" href="/about">About Us</Link>
           <Link className="text-sm hover:underline" href="/resources">Resources</Link>
           <Link className="text-sm hover:underline" href="/contact">Contact</Link>
+          <Link className="btn btn-primary text-sm" href="/book">Book Now</Link>
         </nav>
       </div>
     </header>
